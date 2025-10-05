ea285ee3209cefa7ddd5e269e1337b01d168e554
GitbyShantanu <Shantanudofficial5@gmail.com>
2025-10-06 03:07:22 +0530

    Role Based Authorization (Admin) added.

--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -38,6 +38,13 @@ public class UserService {
         userRepository.save(user);
     }
 
+    // used when new admin user is saved with credentials
+    public void saveAdmin(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER", "ADMIN"));
+        userRepository.save(user);
+    }
+
     public ArrayList<User> getAllEntries() {
         return new ArrayList<>(userRepository.findAll());
     }
